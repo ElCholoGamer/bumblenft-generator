ec55c5225f65a964e8c9aec36b2d999a8cba7741
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-20 00:33:09 -0500

    refactor(footer): edit text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import { HTMLProps } from 'react';
 
 export const Footer: React.FC<HTMLProps<HTMLElement>> = props => (
 	<footer {...props} className="w-100 text-light text-center p-3">
-		<small>Copyright &copy; {new Date().getFullYear()} Sugoma Inc. All Rights Reserved</small>
+		<small>Website goes hard, feel free to screenshot</small>
 	</footer>
 );
