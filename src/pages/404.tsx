88e78d3697a8b6b0923614568fca41909bb3ac2c
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-19 22:29:53 -0500

    feat(pages): add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Button from 'react-bootstrap/Button';
+import { Layout } from 'components/Layout';
+
+const NotFound: NextPage = () => {
+	const router = useRouter();
+
+	return (
+		<Layout title="Not found">
+			<main className="p-4">
+				<h2 className="my-5">Page not found :(</h2>
+
+				<Link href="/" passHref>
+					<Button>Home</Button>
+				</Link>
+
+				<Button className="mx-4" onClick={() => router.back()} variant="outline-primary">
+					Go back
+				</Button>
+			</main>
+		</Layout>
+	);
+};
+
+export default NotFound;
