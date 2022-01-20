749425006ec7cc031f2607873f9c89cd12845d93
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-20 00:19:01 -0500

    feat(gallery): implement pagination

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,37 +1,74 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import type { PartialResource } from 'lib/types';
+import { useState } from 'react';
 import Link from 'next/link';
+import { getAllUploads } from 'lib/uploads';
+import { toPartialUpload } from 'lib/common/uploads';
 import { Layout } from 'components/Layout';
 import { Centered } from 'components/Centered';
 import { ImageList } from 'components/ImageList';
-import { getAllUploads, toPartialUpload } from 'lib/uploads';
+import axios from 'axios';
+import { StatusButton } from 'components/StatusButton';
 
 interface Props {
-	images: PartialResource[];
+	initialImages: PartialResource[];
+	nextCursor?: string;
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-	const images = await getAllUploads();
+	const { resources, nextCursor } = await getAllUploads();
 	return {
-		props: { images: images.map(toPartialUpload) },
+		props: {
+			initialImages: resources.map(toPartialUpload),
+			nextCursor,
+		},
 	};
 };
 
-const Gallery: NextPage<Props> = ({ images }) => (
-	<Layout title="Gallery">
-		<main className="p-3">
-			{!images.length ? (
-				<Centered>
-					No NFTs have been generated yet.{' '}
-					<Link href="/">
-						<a>Generate one now!</a>
-					</Link>
-				</Centered>
-			) : (
-				<ImageList images={images} />
-			)}
-		</main>
-	</Layout>
-);
+const Gallery: NextPage<Props> = ({ initialImages, nextCursor }) => {
+	const [images, setImages] = useState(initialImages);
+	const [cursor, setCursor] = useState(nextCursor);
+	const [loading, setLoading] = useState(false);
+
+	const handleClick = () => {
+		setLoading(true);
+
+		let url = '/api/uploads';
+		if (cursor) url += `?cursor=${cursor}`;
+
+		axios
+			.get(url)
+			.then(({ data }) => {
+				setImages(prev => [...prev, ...data.resources]);
+				setCursor(data.nextCursor);
+			})
+			.catch(console.error)
+			.finally(() => setLoading(false));
+	};
+
+	return (
+		<Layout title="Gallery">
+			<main className="p-3">
+				{!images.length ? (
+					<Centered>
+						No NFTs have been generated yet.{' '}
+						<Link href="/">
+							<a>Generate one now!</a>
+						</Link>
+					</Centered>
+				) : (
+					<>
+						<ImageList images={images} />
+						{cursor && (
+							<StatusButton className="mx-auto d-block" onClick={handleClick} disabled={loading}>
+								Load more
+							</StatusButton>
+						)}
+					</>
+				)}
+			</main>
+		</Layout>
+	);
+};
 
 export default Gallery;
