f4b41e25c64bdf703ab60f8b7f29b2d365fbd48f
ElCholoGamer <josedanielgrayson@gmail.com>
2022-01-19 20:31:45 -0500

    fix(constants): use path.resolve

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -25,7 +25,7 @@ export const layers: LayerInfo[] = [
 	},
 ];
 
-export const LAYERS_FOLDER = path.join(
+export const LAYERS_FOLDER = path.resolve(
 	process.env.NODE_ENV === 'production' ? __dirname : '.',
 	'layers'
 );
